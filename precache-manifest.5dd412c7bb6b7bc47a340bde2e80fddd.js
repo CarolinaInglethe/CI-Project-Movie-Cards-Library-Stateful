09c4827807275d8d24160f3d147c3f76bf47f8dd
Carol_Inglethe <carolinainglethe@gmail.com>
2021-12-04 17:38:54 -0300

    Updates

---
+++ precache-manifest.5dd412c7bb6b7bc47a340bde2e80fddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895193467f4de2d5580b89c99b59ac00",
+    "url": "/your-app-name/index.html"
+  },
+  {
+    "revision": "f8483cf5d10b023132d5",
+    "url": "/your-app-name/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "f40233cb24e4ada6a08c",
+    "url": "/your-app-name/static/js/2.17aeab05.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/your-app-name/static/js/2.17aeab05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8483cf5d10b023132d5",
+    "url": "/your-app-name/static/js/main.f75c71ba.chunk.js"
+  },
+  {
+    "revision": "66289cf19ee95fdb6fd8",
+    "url": "/your-app-name/static/js/runtime-main.9913358c.js"
+  }
+]);
